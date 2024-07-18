61d0fae6a4a96f9b9cabed108edcac389d085fb7
zhuzihaoIT <ITZhuZihao@163.com>
2024-07-18 22:01:36 +0800

    本地第一次提交，为了看一看是否会在拉取远程数据出现冲突

--- user/src/main/java/com/zhuzihao/AppUser.java
+++ user/src/main/java/com/zhuzihao/AppUser.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class AppUser {
     public static void main(String[] args) {
         SpringApplication.run(AppUser.class);
+        System.out.println("hahhah");
     }
 }
